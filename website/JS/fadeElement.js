33fdbc9b532aaab4108e36968a71a74719429068
Man <lad4011@students.mpsaz.org>
2025-04-02 11:30:41 -0700

    Add files via upload

---
+++ website/JS/fadeElement.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hiddenElements = document.querySelectorAll('.hidden');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    hiddenElements.forEach(element => {
+        observer.observe(element);
+    });
+});
